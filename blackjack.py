faa5e12f970dec9489a89bbeaee20d76d4922811
Lucas <66139861+hoffenbauer@users.noreply.github.com>
2021-11-16 11:32:50 -0300

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,8 @@
 """
-MODULE DOCSTRING
+This is blackjack game meant to be played by a player against the
+computer. At its current state, it's a single player game and uses
+the most basic rules of blackjack; i.e., there's no split, double
+down etc.
 """
 from random import shuffle
 from itertools import product
@@ -61,29 +64,29 @@ class Player:
         """
         self.score += deck.cards[-1].value
         self.cards.append(deck.cards.pop(-1))
-        
+
     def place_bet(self):
         """
         Asks the player to place a bet. The ammount cannot be higher than
         the curent balance of the player.
         """
         while True:
-            try: 
+            try:
                 self.bet = int(input('Place your bet: '))
                 if self.bet <= self.balance:
                     break
-                else:
-                    clear_output()
-                    print (f'Your bet cannot exceed your balance (${self.balance}).')
-            except:
+
+                clear_output()
+                print (f'Your bet cannot exceed your balance (${self.balance}).')
+            except ValueError:
                 clear_output()
                 print('Choose a valid number.')
         self.balance -= self.bet
-        
+
 def initial_deal():
     """
     Deals two cards for both the gambler and the dealer.
-    Both cards of the gambler are revealed, but only the 
+    Both cards of the gambler are revealed, but only the
     top one of the dealer.
     """
     clear_output()
@@ -94,7 +97,7 @@ def initial_deal():
         gambler.get_card()
         dealer.get_card()
     show_table()
-    
+
 def show_table():
     """
     Shows the cards drawn by both the dealer and the player. The
@@ -103,12 +106,12 @@ def show_table():
     print('DEALER')
     for card in dealer.cards[1:]:
         print(card)
-        
+
     print('\nGAMBLER')
-    for card in gambler.cards:      
+    for card in gambler.cards:
         print(card)
     print(gambler.score)
-                
+
 def show_table_end():
     """
     Shows the cards drawn by both the dealer and the player. The
@@ -119,77 +122,77 @@ def show_table_end():
     for card in dealer.cards:
         print(card)
     print(dealer.score)
-    
+
     print('\nGAMBLER')
-    for card in gambler.cards:      
+    for card in gambler.cards:
         print(card)
     print(gambler.score)
-    
+
 def hit_stand():
     """
     Asks the gambler if he wishes to take another card
-    (hit) or not (stand). If hits, he receives another 
+    (hit) or not (stand). If hits, he receives another
     card. If he stands, no more cards are dealt for him
-    and the dealer must draw cards until he reachs a 
-    value of 17 or above. 
+    and the dealer must draw cards until he reachs a
+    value of 17 or above.
     """
     stance = ''
     while stance not in ('h', 's'):
         stance = input("Hit or stand? (H/S) ").lower()
-        if stance == 's':
+        if stance == 'h':
+            stance = ''
+            clear_output()
+            gambler.get_card()
+            show_table()
+        elif stance == 's':
             clear_output()
             while dealer.score <= 17:
                 dealer.get_card()
             show_table_end()
             return stance
-        else:
-            stance = ''
-            clear_output()
-            gambler.get_card()
-            show_table()    
 
 def result():
     """
-    Compares the score of the gambler and the dealer for 
+    Compares the score of the gambler and the dealer for
     a winner or a tie (push). If the gambler gets 21 at
     the initial draw, he has a blackjack. If the player
     exceeds 21, it's a bust.
     """
     if gambler.score == dealer.score:
-        result = 'push'
+        match_result = 'push'
     elif gambler.score == 21:
-        result = 'blackjack'
+        match_result = 'blackjack'
         gambler.balance += (gambler.bet+dealer.bet)
     elif gambler.score > 21:
-        result = 'gambler_busts'
+        match_result = 'gambler_busts'
     elif dealer.score > 21:
-        result = 'dealer_busts'
+        match_result = 'dealer_busts'
         gambler.balance += (gambler.bet+dealer.bet)
     elif gambler.score > dealer.score:
-        result = 'gambler_wins'
+        match_result = 'gambler_wins'
         gambler.balance += (gambler.bet+dealer.bet)
     elif gambler.score < dealer.score:
-        result = 'dealer_wins'
-    return result
-    
+        match_result = 'dealer_wins'
+    return match_result
+
 def winner():
     """
-    Adds the respective ammount of money to players' 
+    Adds the respective ammount of money to players'
     balance based on the result of the round.
     """
     if result() == 'push':
         gambler.balance += gambler.bet
         dealer.balance += dealer.bet
-        
+
     elif result() in ('blackjack', 'gambler_wins', 'dealer_busts'):
         gambler.balance += (gambler.bet+dealer.bet)
-        
+
     elif result() in ('gambler_busts', 'dealer_wins'):
         dealer.balance += (gambler.bet+dealer.bet)
-        
+
 def ace_value():
     """
-    Adjusts the value of ace. Aces are normally valued at 11. If the 
+    Adjusts the value of ace. Aces are normally valued at 11. If the
     player score exceeds 21, it's reduced by 10 to 1 to avoid bust.
     This can only occur for one ace in the player
     """
@@ -199,7 +202,7 @@ def ace_value():
     if dealer.score > 21:
         if 'Ace' in [card.rank for card in dealer.cards]:
             dealer.score -= 10
-        
+
 def replay():
     """
     Clears the scores and the hands of the players.
